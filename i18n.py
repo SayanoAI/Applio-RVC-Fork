8a829714d7734f4a48632f707202392ebf3c6019
DeiantV <vidaldiscord1@gmail.com>
2023-09-13 16:02:25 -0500

    Fix language detection
    Co-Authored-By: Aitor Emper <74368135+aitronssesin@users.noreply.github.com>

--- i18n.py
+++ i18n.py
@@ -16,18 +16,38 @@ class I18nAuto:
 
     Examples
     --------
-    >>> i18n = I18nAuto('en_US')
+    >>> i18n = I18nAuto()
     >>> i18n.print()
     Using Language: en_US
     """
     def __init__(self, language=None):
         from locale import getdefaultlocale
         language = language or getdefaultlocale()[0]
-        if not self._language_exists(language):
-            language = "en_US"
 
-        self.language_map = load_language_list(language)
-        self.language = language
+        # Check if a specific language variant exists, e.g., 'es_ES'
+        if self._language_exists(language):
+            self.language = language
+        else:
+            # If not, check if there is a language with the first two characters
+            # matching, e.g., 'es_' for 'es_ES'.
+            lang_prefix = language[:2]
+            for available_language in self._get_available_languages():
+                if available_language.startswith(lang_prefix):
+                    self.language = available_language
+                    break
+            else:
+                # If no match found, default to 'en_US'.
+                self.language = 'en_US'
+
+        self.language_map = load_language_list(self.language)
+
+    @staticmethod
+    def _get_available_languages():
+        from os import listdir
+        from os.path import isfile, join
+
+        language_files = [f for f in listdir("./i18n/locale/") if isfile(join("./i18n/locale/", f))]
+        return [lang.replace(".json", "") for lang in language_files]
 
     @staticmethod
     def _language_exists(language):
@@ -40,4 +60,4 @@ class I18nAuto:
 
     def print(self):
         """Prints the language currently in use."""
-        print(f"Using Language: {self.language}") 
+        print(f"Using Language: {self.language}")
